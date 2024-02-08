964240d42fc5114edda9742ff2a76694448ed195
yamaru0926 <42344397+yamaru0926@users.noreply.github.com>
2024-02-08 16:31:57 +0900

    #8490

--- Traffic Lights/StdAfx.h
+++ Traffic Lights/StdAfx.h
@@ -1,7 +1,7 @@
 // stdafx.h : include file for standard system include files,
 //  or project specific include files that are used frequently, but
 //      are changed infrequently
-//
+
 #if !defined(AFX_STDAFX_H__CDBBB300_8398_4CF1_BF63_0C8EC6E7E9CA__INCLUDED_)
 #define AFX_STDAFX_H__CDBBB300_8398_4CF1_BF63_0C8EC6E7E9CA__INCLUDED_
 //
