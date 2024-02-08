f27a194768e230fccff567c442c28e51289e419e
yamaru0926 <42344397+yamaru0926@users.noreply.github.com>
2024-02-08 16:17:32 +0900

    #8496

--- Traffic Lights/StdAfx.h
+++ Traffic Lights/StdAfx.h
@@ -2,10 +2,10 @@
 //  or project specific include files that are used frequently, but
 //      are changed infrequently
 //
-
+//
 #if !defined(AFX_STDAFX_H__CDBBB300_8398_4CF1_BF63_0C8EC6E7E9CA__INCLUDED_)
 #define AFX_STDAFX_H__CDBBB300_8398_4CF1_BF63_0C8EC6E7E9CA__INCLUDED_
-
+//
 #if _MSC_VER > 1000
 #pragma once
 #endif // _MSC_VER > 1000
