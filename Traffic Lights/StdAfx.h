6c7b626d6bb8876dcddc865a43118570f0b86006
yamaru0926 <42344397+yamaru0926@users.noreply.github.com>
2024-02-08 16:21:17 +0900

    #8496

--- Traffic Lights/StdAfx.h
+++ Traffic Lights/StdAfx.h
@@ -2,7 +2,6 @@
 //  or project specific include files that are used frequently, but
 //      are changed infrequently
 //
-//
 #if !defined(AFX_STDAFX_H__CDBBB300_8398_4CF1_BF63_0C8EC6E7E9CA__INCLUDED_)
 #define AFX_STDAFX_H__CDBBB300_8398_4CF1_BF63_0C8EC6E7E9CA__INCLUDED_
 //
